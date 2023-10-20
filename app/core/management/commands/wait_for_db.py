d1ca6cceba04e82b3d1519bdad486e2a018137da
zackdreitzler <zdreitzler@gmail.com>
2023-10-19 22:40:28 -0400

    Fixed line length of except block in handle function

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,7 +21,9 @@ class Command(BaseCommand):
                 self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write("Database unavailable, waiting for 1 second...")
+                self.stdout.write(
+                    "Database unavailable, waiting for 1 second..."
+                )
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database available!"))
