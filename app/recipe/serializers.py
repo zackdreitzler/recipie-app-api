6d2ab5b57414884e3b7cd7c84bd0c3a0aad41a32
zackdreitzler <zdreitzler@gmail.com>
2023-11-21 21:34:05 -0500

    Finished adding tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,6 +13,13 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
 
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
     class Meta:
         model = Recipe
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
@@ -28,6 +35,19 @@ class RecipeSerializer(serializers.ModelSerializer):
             recipe.tags.add(tag_obj)
 
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update a recipe"""
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
