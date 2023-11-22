5ddb2f6f32dc49fb24e7142f7136f652c9946882
zackdreitzler <zdreitzler@gmail.com>
2023-11-21 21:16:13 -0500

    added ability to create tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -25,3 +25,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Manage tags in the database"""
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve tags for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
