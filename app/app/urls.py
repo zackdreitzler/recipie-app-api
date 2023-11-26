2f5bf13e9b31fb04633bde8d77fa6f2cf55e2506
zackdreitzler <zdreitzler@gmail.com>
2023-11-25 21:00:48 -0500

    Finsh configureing the static file volumes

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,6 +19,8 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -32,3 +34,8 @@ urlpatterns = [
     path("api/user/", include("user.urls")),
     path("api/recipe/", include("recipe.urls")),
 ]
+
+if setting.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
