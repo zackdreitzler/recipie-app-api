22733a4cca8dca9ddb587c3b11b05a47e572b080
zackdreitzler <zdreitzler@gmail.com>
2023-11-25 21:34:13 -0500

    Adding functionality for images

--- app/app/settings.py
+++ app/app/settings.py
@@ -143,3 +143,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "core.User"
 
 REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"}
+
+SPECTACULAR_SETTINGS = {"COMPONENT_SPLIT_REQUEST": True}
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -77,9 +78,19 @@ class ModelTests(TestCase):
         self.assertEqual(str(tag), tag.name)
 
     def test_create_ingredient(self):
+        """Test creating an ingredient"""
         user = create_user()
         ingredient = models.Ingredient.objects.create(
             user=user, name="Ingredient1"
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch("core.models.uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path"""
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "example.jpg")
+
+        self.assertEqual(file_path, f"uploads/recipe/{uuid}.jpg")
